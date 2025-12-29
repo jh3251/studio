2345d17186ec56188256386f912b394f26d3e3e9
Jabed Hossain <jhshifat21@gmail.com>
2025-12-29 04:57:36 +0000

    remove Dashboard Get a quick overview of your financial status.

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -7,11 +7,8 @@ export default function DashboardPage() {
   return (
     <div className="space-y-6">
        <div className="flex justify-between items-center">
-        <div>
-          <h1 className="text-3xl font-bold font-headline tracking-tight">Dashboard</h1>
-          <p className="text-muted-foreground">Get a quick overview of your financial status.</p>
-        </div>
         <NewTransactionButton />
+        <div />
       </div>
       <DashboardClient />
     </div>
