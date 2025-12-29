cd2ee560ef92f75ff89553d6cb72f813b340e6f2
Jabed Hossain <jhshifat21@gmail.com>
2025-12-29 04:56:40 +0000

    new transaction place in left side

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -6,12 +6,12 @@ import { NewTransactionButton } from '@/components/app/new-transaction-button';
 export default function DashboardPage() {
   return (
     <div className="space-y-6">
-      <div className="flex justify-end">
+       <div className="flex justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold font-headline tracking-tight">Dashboard</h1>
+          <p className="text-muted-foreground">Get a quick overview of your financial status.</p>
+        </div>
         <NewTransactionButton />
-      </div>
-      <div>
-        <h1 className="text-3xl font-bold font-headline tracking-tight">Dashboard</h1>
-        <p className="text-muted-foreground">Get a quick overview of your financial status.</p>
       </div>
       <DashboardClient />
     </div>
