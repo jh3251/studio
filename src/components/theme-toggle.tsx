8866cfbbdda66b13bed45c4b49b27a9e640d6077
Jabed Hossain <jhshifat21@gmail.com>
2025-12-30 16:32:01 +0000

    also use switch botton

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -4,20 +4,24 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
-import { Button } from "@/components/ui/button"
+import { Switch } from "@/components/ui/switch"
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme()
 
+  const onCheckedChange = (checked: boolean) => {
+    setTheme(checked ? "dark" : "light")
+  }
+
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-    >
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <div className="flex items-center space-x-2">
+      <Sun className="h-5 w-5" />
+      <Switch
+        checked={theme === "dark"}
+        onCheckedChange={onCheckedChange}
+        aria-label="Toggle theme"
+      />
+      <Moon className="h-5 w-5 text-primary" />
+    </div>
   )
 }
